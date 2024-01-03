387c259aa527137377df3033d72702b699d173ec
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 14:18:40 +0200

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = number % 10
+
+if (num > 5):
+    print("{} and is greater than 5".format(num))
+
+elif (num == 0):
+    print("{} and is zero".format(num))
+
+else :
+    print("{} and is less than 6 and not 0".format(num))
