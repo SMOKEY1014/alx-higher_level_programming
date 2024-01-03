eca8579254507d2187543af18a85001ec83f1515
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 14:33:47 +0200

    end of file

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -7,5 +7,5 @@ if (number > 0):
 elif (number < 0):
     print("{} is negative".format(number))
 
-else :
+else:
     print("{} is zero".format(number))
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,8 @@ number = random.randint(-10000, 10000)
 num = number % 10
 
 if (num > 5):
-    print("{} and is greater than 5".format(num))
-
+    print("{} and is greater than 5".format(num), end="\n")
 elif (num == 0):
-    print("{} and is zero".format(num))
-
-else :
-    print("{} and is less than 6 and not 0".format(num))
+    print("{} and is zero".format(num), end="\n")
+else:
+    print("{} and is less than 6 and not 0".format(num), end="\n")
