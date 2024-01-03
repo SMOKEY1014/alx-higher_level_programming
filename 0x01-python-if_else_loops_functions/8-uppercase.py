744b42219706cf9855e09c65e3ff897b981eda14
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 16:46:08 +0200

    fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(input_str):
     for char in input_str:
         # Check if the character is a lowercase letter
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
 
     # Ensure the number is positive
