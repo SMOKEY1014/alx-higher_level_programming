3f21ecf53245fa82dc95a30b200f05283332c123
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 16:41:21 +0200

    fixed

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    # Initialize result to 1
-    result = 1
-
-    # case when the exponent is negative
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    # Calculating the power using a loop
-    for _ in range(b):
-        result *= a
-
-    # Return the result
-    return result
+    c = a ** b
+    return c
