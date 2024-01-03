4028441ad16ca29142d6343693a85f167f3eb7fb
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 15:53:24 +0200

    tasks

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def islower(c):
+    # Checks if the ASCII value of the character is within the lowercase range
+    return 97 <= ord(c) <= 122
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def uppercase(input_str):
+    for char in input_str:
+        # Check if the character is a lowercase letter
+        if 97 <= ord(char) <= 122:
+            # Convert to uppercase by subtracting 32 from the ASCII value
+            print("{}".format(chr(ord(char) - 32)), end='')
+        else:
+            print("{}".format(char), end='')
+
+    # Print a new line after processing the entire string
+    print()
