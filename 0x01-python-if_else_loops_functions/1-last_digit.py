f738590efeb82392a627215c1c5fd057c42624c8
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-03 14:35:27 +0200

    end of file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 num = number % 10
 
 if (num > 5):
-    print("{} and is greater than 5".format(num), end="\n")
+    print("{} and is greater than 5".format(num))
 elif (num == 0):
-    print("{} and is zero".format(num), end="\n")
+    print("{} and is zero".format(num))
 else:
-    print("{} and is less than 6 and not 0".format(num), end="\n")
+    print("{} and is less than 6 and not 0".format(num))
