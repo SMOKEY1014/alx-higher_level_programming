6e3957a42a71c7043b944d26ff758dffa5357c5c
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-04 16:15:09 +0200

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     args = len(sys.argv)
 
     if args != 4:
-        print(f"Usage: ./100-my_calculator.py {a} <operator> {b}")
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Collect arguments
+    # Extract arguments
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Perform the calculations based on the operator
+    # Perform the operation based on the operator
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     elif operator == '/':
         result = div(a, b)
     else:
-        print("Unknown operator. Use operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     # Print the result
