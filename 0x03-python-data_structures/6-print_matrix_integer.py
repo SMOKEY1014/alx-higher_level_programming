7677aff16f8133538c1f16793cfb8414257a6280
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-08 09:36:35 +0200

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    num_row = len(matrix)
-
     for row in matrix:
-        num_elements = len(row)
-        col = 0  # This will keep track of the column index
-
-        for x in row:
-            if col < num_elements - 1:
-                print("{:d}".format(x), end=" ")
-            else:
-                print("{:d}".format(x), end="")
-            col += 1  # increment column index
+        for elem in row:
+            print("{:d}".format(elem), end=" ")
         print('$')
