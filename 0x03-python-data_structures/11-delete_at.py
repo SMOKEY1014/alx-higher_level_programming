e1d9030af31cc2f827243e5ec7a3c134636aac76
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-08 09:20:10 +0200

    fix

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,5 +5,5 @@ def delete_at(my_list=[], idx=0):
     elif (idx > len(my_list)):
         return (my_list)
     else:
-        new_list = my_list[:idx] + my_list[idx + 1:]
-        return (new_list)
+        del my_list[idx]  # deletes the specified idx from list
+        return (my_list)
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
     padded_tuple_a = tuple_a + (0,) * (len(tuple_b) - len(tuple_a))
     padded_tuple_b = tuple_b + (0,) * (len(tuple_a) - len(tuple_b))
 
-    new_tuple = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
+#   new_tuple = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
+    new_tuple = padded_tuple_a[0]+padded_tuple_b[0], padded_tuple_a[1]+padded_tuple_b[1]
     return (new_tuple)
