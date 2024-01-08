f6ee1c591b9dbe6545b5e00f94eda9f6463b4ac4
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-01-08 10:22:02 +0200

    fix2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     padded_tuple_a = tuple_a + (0,) * (len(tuple_b) - len(tuple_a))
     padded_tuple_b = tuple_b + (0,) * (len(tuple_a) - len(tuple_b))
 
-#   new_tuple = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
-    new_tuple = tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1]
+    new_tuple = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
     return (new_tuple)
