517464320cc6477bee465dddc1d24077974ff49e
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-12 15:30:50 +0200

    advanced

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -5,4 +5,4 @@
 -- 10  2
 -- 8   1
 
-SELECT score FROM second_table, COUNT(score) AS 'number' GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score ORDER BY number DESC;
