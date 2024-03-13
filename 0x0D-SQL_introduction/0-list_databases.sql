c8cc030afab323d5a3cd05e07cb4edb2fcc3849d
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 11:54:00 +0200

    documentation

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,1 +1,3 @@
+--  a script that lists all databases of your MySQL server.
+
 SHOW DATABASES;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,4 @@
+--  a script that deletes the database hbtn_0c_0 in your MySQL server.
+
 DROP DATABASE IF EXISTS hbtn_0c_0;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
