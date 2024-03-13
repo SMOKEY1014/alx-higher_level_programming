cf407a23b60168c4320c2cfe43620d5d615c67db
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 14:12:49 +0200

    test

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,7 +9,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa; -- Switch to the newly created or existing database
 CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
