0a7b31f097c0527f7b81863dc203118cbf11f10e
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 14:48:14 +0200

    test

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -10,6 +10,8 @@
 -- the database name will be passed as an argument of the mysql command
 -- if a show doesn’t have a genre, display NULL
 
-USE hbtn_0d_tvshows;
-SELECT tv_shows.title, tv_show_genres.genre_id 
-    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
