9d2a33c1de1a510a71a512d60ec8d7d1d57c3d0e
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 14:42:21 +0200

    test

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -10,6 +10,5 @@
 -- lists all shows in hbtn_0d_tvsows that have at least 1 genre linked
 
 USE hbtn_0d_tvshows;
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+SELECT tv_shows.title, tv_show_genres.genre_id
     ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
