b7f3cff4c22c467c035fe2569e8c576df8ff5f97
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 15:06:10 +0200

    test

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,6 +10,11 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 USE hbtn_0d_tvshows;
-SELECT tv_genres.name WHERE tv_shows.title = Dexter
-FROM tv_show 
-ORDER BY tv_genres.name ASC;
+SELECT tv_genres.name
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+     WHERE tv_shows.title = "Dexter"
+  ORDER BY tv_genres.name ASC;
