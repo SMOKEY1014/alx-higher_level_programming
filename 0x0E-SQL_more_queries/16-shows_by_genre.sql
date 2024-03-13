93e770398757e5a2706503a4ce6653386743c029
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-03-13 15:20:48 +0200

    test

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -12,8 +12,8 @@
 
 SELECT tv_shows.title, tv_genres.name
       FROM tv_genres
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
         ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows
+LEFT JOIN tv_shows
 	ON tv_show_genres.show_id = tv_shows.id
   ORDER BY tv_shows.title, tv_genres.name ASC;
