1788cb3dbdc10f2fcfc298754c7a6861c01fd985
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-08 11:57:04 +0200

    add function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,14 +2,16 @@
 
 const arg1 = process.argv[2];
 
-const num1 = parseInt(arg1);
+const a = parseInt(arg1);
 
 const arg2 = process.argv[3];
 
-const num2 = parseInt(arg2);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-	console.log(num1 + num2);
-} else {
-	console.log(NaN);
+const b = parseInt(arg2);
+function add(a, b) {
+	if (!isNaN(a) && !isNaN(b)) {
+		console.log(a + b);
+	} else {
+		console.log(NaN);
+	}
 }
+add(a, b);
