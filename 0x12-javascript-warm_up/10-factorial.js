2b7958966be88c4a460ea8f5c63f8fc780f7c821
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-08 12:11:35 +0200

    factorial recursive function

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+function factorial(n) {
+    if (isNaN(n) || n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+const arg = process.argv[2];
+
+const num = parseInt(arg);
+
+const result = factorial(num);
+console.log(result);
+
