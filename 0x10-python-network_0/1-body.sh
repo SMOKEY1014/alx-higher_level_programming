e2b2bc1328b44aa9ccbce11c97e66c9032393b79
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 09:17:39 +0200

    A Bash script that takes in a URL, sends a GET request to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# A Bash script that takes in a URL, sends a GET request to the URL,
+# and displays the body of the response
+# Display only body of a 200 status code response
+
 
 # Check if the user provided a URL as an argument
 if [ $# -ne 1 ]; then
