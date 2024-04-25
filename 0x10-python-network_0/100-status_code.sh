07ab96571a6e63adc52f7fd481a61a9ab6d1c765
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:58:11 +0200

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL,
-curl -s -L -o text.txt -w "%{http_code}" "$1" | rm -f text.txt | echo "$status_code"
+curl -so /dev/null -w "%{http_code}" "$1"
