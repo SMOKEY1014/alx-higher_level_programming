e80f0a2c2f9fba3cbdbdc517c4cb9b11f9b587b5
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:51:10 +0200

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a POST request to the passed URL,
-# and displays the body of the response
-# A variable email must be sent with the value `test@gmail.com``
-# A variable subject must be sent with the value `I will always be here for PLD`
-
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Define the URL from the first argument
-url="$1"
-
-# Define variables for email and subject
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send a POST request with the variables and display the response body
-curl -s -X POST -d "email=$email&subject=$subject" "$url"
+# A Bash script that takes in a URL, sends a POST request with data to the passed URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
