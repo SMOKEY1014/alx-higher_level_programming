a6041d6633c7a75f13a655f2b3198a57b754bd1a
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:35:52 +0200

    errorfixing for checker

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a DELETE request to the URL passed as
-# the first argument and displays the body of the response
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-status_code=$(curl -s -L -o text.txt -w "%{http_code}" "$1")
-
-if [ "$status_code" == "200" ]; then
-    curl -s -X DELETE "$1"
-fi
-rm -f text.txt
+curl -s -X DELETE "$1"
