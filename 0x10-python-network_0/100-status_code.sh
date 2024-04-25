6161eec2d1aa3189ff1c783239bc0d22c33cfc42
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:57:03 +0200

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-# Display only body of a 200 status code response
-
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-status_code=$(curl -s -L -o text.txt -w "%{http_code}" "$1")
-rm -f text.txt
-echo "$status_code"
+curl -s -L -o text.txt -w "%{http_code}" "$1" | rm -f text.txt | echo "$status_code"
