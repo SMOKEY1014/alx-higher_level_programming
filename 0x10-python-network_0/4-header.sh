327a70d1f9c25277009ec247f22eaa43afc2dcd8
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:52:13 +0200

    4-header.sh00.1

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that sends a GET request with header X-School-User-Id to the URL, and displays the body of the response
-curl -sLH "X-School-User-Id: 98" "$url"
+curl -sLH "X-School-User-Id: 98" "$1"
