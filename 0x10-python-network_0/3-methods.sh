9e01e3e54a6f4a3e3fcf39c244f22f16e800889c
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:46:23 +0200

    3-methods.sh.001

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i "Allow:" | cut -d ':' -f2 | tr -d ''
+curl -sI "$1" | grep "Allow:" | cut -d ' ' -f2-
