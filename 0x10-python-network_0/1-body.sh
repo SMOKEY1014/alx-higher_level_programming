0208dc710833ab953a12b7c948ed056b097d866e
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 12:34:03 +0200

    errorfixing for checker

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-# Display only body of a 200 status code response
-
-
-# Check if the user provided a URL as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-status_code=$(curl -s -L -o text.txt -w "%{http_code}" "$1")
-
-if [ "$status_code" == "200" ]; then
-    cat text.txt
-fi
-rm -f text.txt
+# send a GET request to an URL with curl, and display the body of the response
+curl -s -L -X GET "$1"
