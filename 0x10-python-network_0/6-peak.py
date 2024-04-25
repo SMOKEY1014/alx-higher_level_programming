2e8f7630ed37477950d6831a084074eb25270479
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-25 11:56:34 +0200

    python

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
