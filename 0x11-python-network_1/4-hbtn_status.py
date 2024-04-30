42e802573bf15e9b910e76fafee56804209c7b6c
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-30 13:54:08 +0200

    documentation

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import requests
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status
+    Using the package 'requests'
+"""
 
 if __name__ == "__main__":
-
     url = 'https://alx-intranet.hbtn.io/status'
 
     response = requests.get(url)
