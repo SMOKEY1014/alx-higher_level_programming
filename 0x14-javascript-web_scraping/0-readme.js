a81e85d4083bdaac40534d560bf813f233eb454b
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-05-21 11:06:54 +0200

    js script that reads other file contents

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const path = process.argv[2];
+
+if (!path) {
+  console.error("Usage: node 0-readme.js <file-path>");
+  process.exit(1);
+}
+
+fs.readFile(path, "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
