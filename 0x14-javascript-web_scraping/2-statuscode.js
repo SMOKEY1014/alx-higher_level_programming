da961567a779455f7ed25005ba7b68d8d568eec9
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-05-21 11:25:11 +0200

    js script that gets the url statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require("request");
+const url = process.argv[2];
+
+if (!url) {
+  console.error("Usage: node 2-statuscode.js <URL>");
+  process.exit(1);
+}
+
+request(url, (err, responce) => {
+  if (err) {
+    console.error("Error:", err);
+    return;
+  }
+  console.log("code:", responce.statusCode);
+});
