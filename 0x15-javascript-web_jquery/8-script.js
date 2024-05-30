eb7c278f904c9458241b5eeefb109485a8041f8a
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-05-30 12:44:33 +0200

    $ - selector header movie.append function advanced

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // code must be executed when the document is ready and fully loaded
   $.get(
-    "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+    "https://swapi-api.alx-tools.com/api/films/?format=json",
     function (data) {
       $.each(data.results, function (index, movie) {
         $("UL#list_movies").append("<li>" + movie.title + "</li>");
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // code must be executed when the document is ready and fully loaded
+  $.get("https://hellosalut.stefanbohacek.dev/?lang=fr", function (data) {
+    $("#character").text(data.name);
+  });
+});
